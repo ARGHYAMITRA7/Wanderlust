984ed321529eb657399d85ffd7e9c4f34d1b1e20
ARGHYAMITRA7 <way2raniganj.2019@gmail.com>
2026-07-28 20:37:45 +0530

    Initial redirection issue fixed

--- app.js
+++ app.js
@@ -92,6 +92,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
 // Works in Express 5
 app.all(/.*/, (req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
